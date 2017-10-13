da341b79eb356f0f9288f441cedbcf0ded459e27
Ehirim Emeka <bluegrayonics@gmail.com>
2017-10-13 11:53:40 +0100

    13th October sidebar working

--- src/app/dashboard-module/dashboard/dashboard-sidebar/dashboard-sidebar.component.ts
+++ src/app/dashboard-module/dashboard/dashboard-sidebar/dashboard-sidebar.component.ts
@@ -1,15 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit,AfterViewChecked } from '@angular/core';
+
+declare var $: any;
+declare var jQuery: any;
 
 @Component({
   selector: 'app-dashboard-sidebar',
   templateUrl: './dashboard-sidebar.component.html',
   styleUrls: ['./dashboard-sidebar.component.css']
 })
-export class DashboardSidebarComponent implements OnInit {
+export class DashboardSidebarComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
-  ngOnInit() {
+  
+
+
+  
+  createScript(url){
+    let mainScript = document.createElement('script');
+    mainScript.src = url;
+    mainScript.type = ('text/javascript');
+    document.getElementsByTagName('head')[0].appendChild(mainScript);
+
   }
 
+
+
+
+
+
+
+  ngOnInit() {
+
+
+  }
+
+  // ngAfterViewChecked(){
+  //   this.createScript('assets/js/main.js');
+  //   this.createScript('assets/js/vendor/jquery.slimscroll.js');
+  //   this.createScript('assets/js/vendor/jquery.min.js');
+  // }
+
+  ngAfterViewInit() {
+  //  var  mainUrl = 'assets/js/main.js';
+  //  var slimScrollUrl = 'assets/js/vendor/jquery.slimscroll.js';
+   
+  this.createScript('assets/js/main.js');
+  this.createScript('assets/js/vendor/jquery.slimscroll.js');
+    
+  this.createScript('assets/js/vendor/jquery.min.js');
+ 
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
